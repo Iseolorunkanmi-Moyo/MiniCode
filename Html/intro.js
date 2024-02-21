1bbd095e1647762778110d41a2136f46b67bd7a2
Oluwatomisin <kanmitomi@gmail.com>
2024-02-21 22:24:59 +0100

    first commit

--- Html/intro.js
+++ Html/intro.js
@@ -3,42 +3,34 @@ var count = 1;
 document.querySelector("#dd-btn").addEventListener("click", show);
 
 function show() {
-    
-    count++
-
-    if (count % 2 === 0){
-
-        document.getElementById("dd-content").style.display = "block";
-
-    }
-
-    else if (count % 2 === 1){
-
-        document.getElementById("dd-content").style.display = "none";
-
-    }
-    
+    document.getElementById("side-nav-bar").style.animationName = "slide"
+    document.getElementById("side-nav-bar-main-div").style.display = "block";
+    document.getElementById("side-nav-bar").style.display = "flex"            
 }
 
-var count2 = 1;
-
-document.querySelector("#right-btn").addEventListener("click", show2);
-
-function show2() {
+document.getElementById("side-nav-bar-main-div").addEventListener("click", function(){
     
-    count2++
-
-    if (count2 % 2 === 0){
-
-        document.getElementById("side-nav-bar").style.display = "flex";
-        document.getElementById("right-btn").style.backgroundImage = "url(/Images/left-arrow.svg)";
-    }
-
-    else if (count2 % 2 === 1){
-
-        document.getElementById("side-nav-bar").style.display = "none";
-        document.getElementById("right-btn").style.backgroundImage = "url(/Images/right-arrow.svg)";
-
-    }
+    setTimeout(() => {
+        document.getElementById("side-nav-bar-main-div").style.display = "none"
+        document.getElementById("dd-content").style.display = "none";
     
+    }, 400);
+
+    document.getElementById("side-nav-bar").style.animationName = "slide-out" 
+   
+})
+
+var users = ["moyo", "jay"]
+
+var account = localStorage.getItem("nam");
+
+if (users.includes(account)){
+    
+    document.getElementById("log").innerHTML = "Out"
+}
+
+document.getElementById("logs").addEventListener("click", clear);
+
+function clear (){
+    localStorage.removeItem("nam")
 }
